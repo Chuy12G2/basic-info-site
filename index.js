14f307ca36885286e4e5de163734f3dee0fc43f9
jesus <chuygs12@gmail.com>
2023-08-04 13:57:53 -0600

    finish server with the different urls

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 const http = require('http')
-const fc = require('fs')
+const fs = require('fs')
 
-http.createServer()
+http.createServer((req, res) => {
+    if(req.url === '/'){
+        fs.readFile('index.html', (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.write(data)
+            return res.end()
+        })
+    } else if (req.url === '/about'){
+        fs.readFile('about.html', (err, data) => {
+            if(err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.write(data)
+            return res.end()
+        })
+    } else if (req.url === '/contact'){
+        fs.readFile('contact-me.html', (err, data) => {
+            if(err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.write(data)
+            return res.end()
+        })
+    } else {
+        fs.readFile('404.html', (err, data) => {
+            if(err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.write(data)
+            return res.end()
+        })
+    }
+}).listen(8080, () => console.log('listening to port 8080'))
